71aba36e959b0adb5707125a3358490c207989e5
SirFrank0 <frankseb04@gmail.com>
2022-12-01 20:06:30 -0500

    js error corrected

--- script.js
+++ script.js
@@ -18,11 +18,12 @@ $form.addEventListener('submit', handleSubmit)
 async function handleSubmit(event){
     event.preventDefault()
     const form = new FormData(this)
-    const await = fetch(this.action,
+    const response = await fetch(this.action, {
     method: this.method,
     body: form,
     headers: {
         'Accept': 'application/json'
+    }
     })
 
     if (response.ok){
